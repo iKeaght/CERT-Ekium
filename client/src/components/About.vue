36098782459fe4a02e5b02c103eb6056bd76a4d1
matheoekium <matheo.noel@ekium.eu>
2021-04-19 11:56:46 +0200

    url view page

--- client/src/components/About.vue
+++ client/src/components/About.vue
@@ -1,11 +1,23 @@
 <template>
-  <panel title="About Page"></panel>
+
+  <panel title="About Page">
+  <slot >
+    <div>zaa</div> 
+  <a window.open="('http://www.google.com','_blank')">Google</a>
+  <a @click="goToExternalPage()">Google</a>
+  </slot>
+  </panel>
 </template>
 
 <script>
 import Panel from '@/components/Panel'
 export default {
   components: { Panel },
+  methods:{
+    goToExternalPage(){
+window.open('http://www.google.com',"_blank")
+    }
+  }
 
 }
 </script>
